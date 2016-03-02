a3e3d2049f74fdcdc636688bf14c1d25a27a85f9
Maksim Karpychev <kodermax@gmail.com>
2016-03-02 10:37:50 +0300

    add title for Avatar

--- components/avatar/Avatar.jsx
+++ components/avatar/Avatar.jsx
@@ -4,11 +4,10 @@ import style from './style';
 
 const Avatar = ({children, className, icon, image, title, ...other}) => {
   return (
-    <div data-react-toolbox='avatar' className={`${style.avatar} ${className}`} {...other}>
+    <div data-react-toolbox='avatar' className={`${style.avatar} ${className}`} title={title} {...other}>
       {children}
       {typeof image === 'string' ? <img className={style.image} src={image} title={title} /> : image}
       {typeof icon === 'string' ? <FontIcon className={style.letter} value={icon} /> : icon}
-      {title ? <span className={style.letter}>{title[0]}</span> : null}
     </div>
   );
 };
--- spec/components/avatar.jsx
+++ spec/components/avatar.jsx
@@ -6,7 +6,7 @@ const AvatarTest = () => (
   <section>
     <h5>Avatars</h5>
     <p>Provide an image source or object, a font icon, children or a title to use its first letter.</p>
-    <Avatar style={{backgroundColor: 'deepskyblue'}} icon="folder" />
+    <Avatar title="super" style={{backgroundColor: 'deepskyblue'}} icon="folder" />
     <Avatar icon={<GithubIcon />}/>
     <Avatar><img src="https://placeimg.com/80/80/animals"/></Avatar>
     <Avatar title="Javier" img="https://placeimg.com/80/80/animals"/>
