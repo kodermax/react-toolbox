cd46099b96cef8c9c8305bbec786be243dbc6539
Maksim Karpychev <kodermax@gmail.com>
2016-03-11 13:02:56 +0300

    Новый компонент AutoSuggest

---
+++ components/autosuggest/Autosuggest.jsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import ClassNames from 'classnames';
+import Input from '../input';
+import style from './style';
+import events from '../utils/events';
+
+export default class Autosuggest extends React.Component {
+  static propTypes = {
+    className: React.PropTypes.string,
+    error: React.PropTypes.string,
+    label: React.PropTypes.string,
+    multiple: React.PropTypes.bool,
+    onChange: React.PropTypes.func,
+    onSelectItem: React.PropTypes.func,
+    source: React.PropTypes.any,
+    template: React.PropTypes.func,
+    value: React.PropTypes.any
+  };
+
+  static defaultProps = {
+    className: '',
+    direction: 'auto',
+    multiple: true,
+    source: {}
+  };
+  state = {
+    direction: this.props.direction,
+    focus: false,
+    query: ''
+  };
+  handleSuggestionHover = (key) => {
+    this.setState({active: key});
+  };
+  select (key, event) {
+    events.pauseEvent(event);
+    this.handleSelect(key, event);
+  }
+  suggestions () {
+    return this.props.source;
+  }
+  renderSuggestions () {
+    const suggestions = [...this.suggestions()].map((item, key) => {
+      const className = ClassNames(style.suggestion, {[style.active]: this.state.active === key});
+      return (
+        <li
+          key={key}
+          className={className}
+          onMouseDown={this.select.bind(this, key)}
+          onMouseOver={this.handleSuggestionHover.bind(this, key)}
+        >
+          {this.props.template ? this.props.template(item) : item.value}
+        </li>
+      );
+    });
+
+    const className = ClassNames(style.suggestions, {[style.up]: this.state.direction === 'up'});
+    return <ul ref='suggestions' className={className}>{suggestions}</ul>;
+  }
+  getItem (key) {
+    return this.source().get(key);
+  }
+  source () {
+    const { source: src } = this.props;
+    if (src.hasOwnProperty('length')) {
+      return new Map(src.map((item, key) => Array.isArray(item) ? [...item] : [key, item]));
+    } else {
+      return new Map(Object.keys(src).map((item, key) => [key, src[key]]));
+    }
+  }
+  handleSelect = (key, event) => {
+    const query = this.getItem(key);
+    if (this.props.onSelectItem) this.props.onSelectItem(key, query);
+    this.setState({ focus: false, query: query.value }, () => { this.refs.input.blur(); });
+  };
+  handleQueryChange = (value) => {
+    if (this.props.onChange) this.props.onChange(value);
+    this.setState({query: value});
+  };
+  handleQueryFocus = () => {
+    this.refs.suggestions.scrollTop = 0;
+    this.setState({active: '', focus: true});
+  };
+  handleQueryBlur = () => {
+    if (this.state.focus) this.setState({focus: false});
+  };
+  handleQueryKeyUp = (event) => {
+    if (event.which === 13 && this.state.active >= 0) this.select(this.state.active, event);
+    if (event.which === 27) this.refs.input.blur();
+    if ([40, 38].indexOf(event.which) !== -1) {
+      const suggestionsKeys = [...this.suggestions().keys()];
+      let index = suggestionsKeys.indexOf(this.state.active) + (event.which === 40 ? +1 : -1);
+      if (index < 0) index = suggestionsKeys.length - 1;
+      if (index >= suggestionsKeys.length) index = 0;
+      this.setState({active: suggestionsKeys[index]});
+    }
+  };
+  render () {
+    const {error, label, ...other} = this.props;
+    const className = ClassNames(style.root, {
+      [style.focus]: this.state.focus
+    }, this.props.className);
+
+    return (
+      <div data-react-toolbox='autosuggest' className={className}>
+        <Input
+          {...other}
+          ref='input'
+          className={style.input}
+          error={error}
+          label={label}
+          onBlur={this.handleQueryBlur}
+          onChange={this.handleQueryChange}
+          onFocus={this.handleQueryFocus}
+          onKeyUp={this.handleQueryKeyUp}
+          value={this.state.query}
+        />
+        {this.renderSuggestions()}
+      </div>
+    );
+  }
+}
---
+++ spec/components/autosuggest.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import Autosuggest from '../../components/autosuggest';
+import $ from 'jquery';
+const usersObject = {
+  '123': {'value':'Javi Velasco','position':'UI Developer', 'img':'https://avatars2.githubusercontent.com/u/1634922?v=3&s=48'},
+  '321': {'value': 'Javi Jiménez', 'position':'Director', 'img':'https://avatars2.githubusercontent.com/u/559654?v=3&s=460'}
+};
+class AutosuggestTest extends React.Component {
+  state = {
+    user: '321'
+  };
+
+  componentDidMount(){
+    this.fetchData();
+  }
+  fetchData = (q) => {
+    if (!q)
+      q = '';
+    $.get(`http://dev.pharm.local/api/users?q=` + q, function(result){
+      this.setState({usersObject: result});
+    }.bind(this));
+  };
+  handleChange = (query) => {
+    this.fetchData(query);
+  };
+  handleSelect = (key, value) => {
+    console.log(value);
+  };
+  customUser = (item) => {
+    const containerStyle = {
+      display: 'flex',
+      flexDirection: 'row'
+    };
+
+    const imageStyle = {
+      display: 'flex',
+      width: '32px',
+      height: '32px',
+      flexGrow: 0,
+      marginRight: '8px',
+      backgroundColor: '#ccc'
+    };
+
+    const contentStyle = {
+      display: 'flex',
+      flexDirection: 'column',
+      flexGrow: 2
+    };
+    const positionStyle = {
+      fontSize:'11px',
+    };
+
+    return (
+      <div style={containerStyle}>
+        {item.img ? <img src={'http://dev.pharm.local/' + item.img} style={imageStyle}/> : '' }
+        <div style={contentStyle}>
+          <span>{item.value}</span>
+          <span style={positionStyle}>{item.position}</span>
+        </div>
+      </div>
+    );
+  };
+
+  render () {
+    return (
+      <section>
+        <h5>Autosuggest</h5>
+        <Autosuggest
+          label="Choose a user"
+          multiple={false}
+          onChange={this.handleChange}
+          onFocus={this.handleQueryFocus}
+          onSelectItem={this.handleSelect}
+          source={this.state.usersObject}
+          template={this.customUser}
+        />
+      </section>
+    );
+  }
+}
+
+export default AutosuggestTest;
